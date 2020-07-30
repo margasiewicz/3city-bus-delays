e0c8072cba9635f14a7ce3765b32a45573f3bb21
Marcin Gasiewicz <margasiewicz@gmail.com>
2020-07-30 12:47:30 +0200

    add working search engine

--- README.md
+++ README.md
@@ -2,4 +2,6 @@
 Initial commit
 TODO: 
 [ ] change json_delay(self, stop_id) to take stop name as argument
-[ ] add autocomplete search
+[x] add autocomplete search
+[ ] add submit form
+[ ] put all css in on place
--- app.py
+++ app.py
@@ -10,10 +10,22 @@ class SearchForm(Form):
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/index', methods=['GET', 'POST'])
 def index():
+    if request.method == "POST":
+        number = request.form['autocomp']
+        return redirect(url_for('bus', number=number))
+    else:
+        form = SearchForm(request.form)
+        data = Timetable()
+        data = json.loads(data.json_delay(36050))
+        return render_template('index.html', data=data, form=form)
+
+@app.route('/bus/<number>', methods=['GET','POST'])
+def bus(number):
     form = SearchForm(request.form)
     data = Timetable()
-    data = json.loads(data.json_delay(36050))
-    return render_template('index.html', data=data, form=form)
+    data = json.loads(data.json_delay(number))
+    return render_template('bus.html', data=data, form=form)
+
 
 @app.route('/_autocomplete', methods=['GET'])
 def autocomplete():
