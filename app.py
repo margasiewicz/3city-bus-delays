941d05d8a72bd7933d0a781485e765f27a0abff5
Marcin Gasiewicz <margasiewicz@gmail.com>
2020-07-30 12:11:07 +0200

    add autocomplete form

--- app.py
+++ app.py
@@ -1,16 +1,28 @@
 from rozklad import Timetable
-from flask import Flask, render_template, request, redirect, json, url_for
-
+from flask import Flask, render_template, request, redirect, url_for, Response
+import json
+from wtforms import TextField, Form
 app = Flask(__name__)
 
-@app.route('/')
-@app.route('/index')
-def index():
-    data = Timetable()
-    data = json.loads(data.json_delay(39100))
-    colnames=['route_id', 'headsign', 'delay_mins']
-    return render_template('index.html', data=data, colnames=colnames)
+class SearchForm(Form):
+    autocomp = TextField('Insert Bus Stop', id='stop_autocomplete')
 
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/index', methods=['GET', 'POST'])
+def index():
+    form = SearchForm(request.form)
+    data = Timetable()
+    data = json.loads(data.json_delay(36050))
+    return render_template('index.html', data=data, form=form)
+
+@app.route('/_autocomplete', methods=['GET'])
+def autocomplete():
+    with open('stops.json') as json_file:
+        stops = json.load(json_file)
+    
+    stops = [item for item in stops]
+
+    return Response(json.dumps(stops), mimetype='application/json')
 if __name__ == '__main__':
     app.debug = True
     app.run()
